e57c5756aba9ee0308dd393671f9110e9ec7a8ac
Kshitij K <kshitijkk15@gmail.com>
2024-07-13 18:21:54 +0530

    control strucures

--- controlStructures/switchtest.cpp
+++ controlStructures/switchtest.cpp
@@ -3,11 +3,11 @@
 int main(void)
 {
     short age;
-    
+    float rate;
     printf("Age: ");
     scanf("%hd", &age);
     
-    float rate;
+   
     /*
     if(age == 16)
         rate = 60;
@@ -21,5 +21,29 @@ int main(void)
         rate = 50;
     */
 
-    return 0;
+   switch(age){
+
+case 16:
+			rate=60;
+			break;
+case 18:
+			rate=80;
+			break;
+case 21:
+			rate=120;
+			break;
+case 50:
+			rate=250;
+			break;
+default:
+			rate=50;
+			
+
+
 }
+
+printf("TOtal gift amount=%.2lf\n",age*rate);
+
+}
+// #wap to take age and output voting
+
